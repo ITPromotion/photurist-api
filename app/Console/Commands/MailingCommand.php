8f3e142d102090320920d3215a175693160fcb94
vakaryuk <vakaryuk@itpromotion.com.ua>
2021-10-20 15:52:47 +0300

    fix

--- app/Console/Commands/MailingCommand.php
+++ app/Console/Commands/MailingCommand.php
@@ -50,7 +50,7 @@ class MailingCommand extends Command
 
             $lastMailing = $postcard->lastMailing();
 
-            if((!$lastMailing)||(Carbon::parse($lastMailing->start)->addMinutes(env('INTERVAL_STEP',60))<Carbon::now())){
+            if((!$lastMailing)||(Carbon::parse($lastMailing->start)->addMinutes(env('INTERVAL_STEP',5))<Carbon::now())){
 
                 $userIds = DB::table('postcards_mailings')
                             ->where('postcard_id', $postcard->id)
