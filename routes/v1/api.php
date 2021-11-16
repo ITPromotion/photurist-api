c44a9fcc420a0bd71da8cef873865ceaf2d9cf23
Sergii <dragnil589@gmail.com>
2021-11-16 16:13:28 +0200

    fix crop video

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -3,8 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientApp\Api\v1\LoginController;
-use FFMpeg\FFMpeg;
-use FFMpeg\FFProbe;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,29 +14,6 @@ use FFMpeg\FFProbe;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/test', function (Request $request) {
-    $ffmpeg = FFMpeg::create();
-    $ffprobe = FFProbe::create();
-    $video = $ffmpeg->open('storage/m.mp4');
-    $video_dimensions = $ffprobe->
-    streams( 'storage/m.mp4' )   // extracts streams informations
-    ->videos()                      // filters video streams
-    ->first()                       // returns the first video stream
-    ->getDimensions();
-    $width = $video_dimensions->getWidth();
-    $height =  $video_dimensions->getHeight();
-    $width_b = $height < $width;
-    $height_b = $height > $width;
-    $video->filters()->custom('crop=420:420:x:y,scale=w=1920:h=420');
-
-    // $video->filters()
-
-    //                 ->synchronize();
-    // $video->filters()
-
-    $video->save(new \FFMpeg\Format\Video\X264(), 'storage/test231.mp4');
-    return $height;
-});
 /* Checking phone number */
 Route::get('/check-mobile', [LoginController::class, 'checkMobile']);
 
