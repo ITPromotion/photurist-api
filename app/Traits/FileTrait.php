5896631b67994daac70ff45e4c49115567c7bdf6
Sergii <dragnil589@gmail.com>
2021-11-16 22:19:08 +0200

    fix crop options video

--- app/Traits/FileTrait.php
+++ app/Traits/FileTrait.php
@@ -67,7 +67,7 @@ trait FileTrait
                         $scaleH = (integer)$height > $width  ? $height : $size;
 
 
-                        $video->filters()->custom("crop=184:184:$xy:$xy,scale=w=184:h=184");
+                        $video->filters()->custom("crop=184:184,scale=w=184:h=184");
 
                         $video->save(new \FFMpeg\Format\Video\X264(), 'storage/'.$folder."/$value/".$videoName);
                     } catch (\Throwable $th) {
