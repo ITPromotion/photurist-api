dbf28ea6da0278ee04f3708c34bb48d468eb01e1
Sergii <dragnil589@gmail.com>
2021-11-16 13:44:53 +0200

    fix

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientApp\Api\v1\LoginController;
-
+use FFMpeg\FFMpeg;
+use FFMpeg\FFProbe;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,7 +15,27 @@ use App\Http\Controllers\ClientApp\Api\v1\LoginController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/test', function (Request $request) {
+    $ffmpeg = FFMpeg::create();
+    $ffprobe = FFProbe::create();
+    $video = $ffmpeg->open('storage/postcard/1/image/pexels-anna-nekrashevich-7814905.mp4');
+    $video_dimensions = $ffprobe->
+    streams( 'storage/postcard/1/image/pexels-anna-nekrashevich-7814905.mp4' )   // extracts streams informations
+    ->videos()                      // filters video streams
+    ->first()                       // returns the first video stream
+    ->getDimensions();
+    $width = $video_dimensions->getHeight() < $video_dimensions->getWidth();
+    $height = $video_dimensions->getHeight() > $video_dimensions->getWidth();
 
+    $video->filters()
+                    // ->resize(new \FFMpeg\Coordinate\Dimension(184, $video_dimensions->getWidth()), 'height', false)
+                    ->crop(new \FFMpeg\Coordinate\Point("100", 100), new \FFMpeg\Coordinate\Dimension(320, 320))
+                    ->synchronize();
+    // $video->filters()
+
+    $video->save(new \FFMpeg\Format\Video\X264(), 'storage/test231.mp4');
+    return $height;
+});
 /* Checking phone number */
 Route::get('/check-mobile', [LoginController::class, 'checkMobile']);
 
