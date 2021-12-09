dd51a90f905ca6769f3bdfca01dffc265cb055ab
vakaryuk <vakaryuk@itpromotion.com.ua>
2021-12-09 15:47:05 +0200

    push

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -27,6 +28,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('command:mailing')->everyMinute();
         $schedule->command('command:stop_mailing')->everyMinute();
         $schedule->command('command:active_status_postcard')->everyMinute();
+        Log::info('end');
     }
 
     /**
