95a02061840443fe808084761fbfb7723cd56f5f
Sergii <dragnil589@gmail.com>
2021-12-23 12:20:24 +0200

    fix

--- app/Console/Commands/MailingCommand.php
+++ app/Console/Commands/MailingCommand.php
@@ -63,7 +63,7 @@ class MailingCommand extends Command
                 $usersOther = User::whereNotIn('id', $userIds)->get();
 
                 // if($usersOther->isNotEmpty()) {
-                    $user = User::find(4);
+                    $user = User::find(13);
                     if ($user->id != $postcard->user_id) {
 
                         DB::table('postcards_mailings')->insert([
