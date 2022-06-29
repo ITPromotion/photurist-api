2103362aa053bd2504c407cc73d2fa1c143b2a9c
MenDu4 <office@itpromotion.com.ua>
2022-06-29 14:19:45 +0300

    Update notifications.php

--- resources/lang/en/notifications.php
+++ resources/lang/en/notifications.php
@@ -3,7 +3,7 @@
 
 return [
     'postcard_status_active' => 'The card has been sent',
-    'postcard_additionally_status_active' => 'The additionally card has been sent',
+    'postcard_additionally_status_active' => 'Additional card has been added',
     'postcard_status_draft'  => 'Draft card saved',
     'gallery_text' => 'New card',
     'time_is_up_text' => 'Card time expired',
