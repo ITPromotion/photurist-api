c4f2df38e7933cb85c8ba5b65111fafeb85dad64
MenDu4 <office@itpromotion.com.ua>
2022-06-29 15:07:15 +0300

    Update notifications.php

--- resources/lang/en/notifications.php
+++ resources/lang/en/notifications.php
@@ -3,13 +3,13 @@
 
 return [
     'postcard_status_active' => 'The card has been sent',
-    'postcard_additionally_status_active' => 'Additional card has been added',
+    'postcard_additionally_status_active' => 'Addional card added',
     'postcard_status_draft'  => 'Draft card saved',
     'gallery_text' => 'New card',
     'time_is_up_text' => 'Card time expired',
     'delete_postcard_text' => 'Card deleted',
     'waiting_time_text' => 'Card timed out',
-    'additional_postcard' => 'Additional card has been added',
+    'additional_postcard' => 'Addional card added',
 
     'add_contacts' => "Added you to contacts",
 ];
