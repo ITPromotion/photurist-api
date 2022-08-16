49a2e1bff6f3fcaac46d930d78bd43803e70b9ba
vakaryuk <vakaryuk@itpromotion.com.ua>
2022-08-16 16:38:55 +0300

    push log

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -30,6 +30,8 @@ class NotificationService {
         $url = 'https://fcm.googleapis.com/fcm/send';
         $FcmToken = $req['tokens'];
 
+        Log::channel('push')->info(['tokens' => $FcmToken]);
+
         $serverKey = env('FIREBASE_SERVER_KEY','AAAAaaY_6k4:APA91bFE0tcfDXkfgeaawi6AtxsjIJj9t-iRCWOPYPglpZHvfWy4VhhMZ0x4lxVB5APqBaKkeQldDplXdUj825lmZFlHvO6qBsFEcfx3MGDONVP2bR7BEHct5xSl35EJ6J4UekzkEqBw');
         $data = [
             "registration_ids" => $FcmToken,
